f0abf092ff74424d7644336babc9424e6d5a4fb5
PC <hngphanminh147@gmail.com>
2020-03-14 12:27:19 +0700

    Create Project

---
+++ Members/1712484/mean-app/src/app/mem-list/mem-list.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-mem-list",
+  templateUrl: "./mem-list.component.html"
+})
+export class MemListComponent {}
---
+++ Members/1712484/mean-app/src/app/mem-list/profile.model.ts
@@ -0,0 +1,6 @@
+export interface MemProfile{
+    studentId: string,
+    name: string,
+    email: string,
+    gitId: string
+}
