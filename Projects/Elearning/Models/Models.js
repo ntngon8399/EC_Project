41df357ee63ff17142a8f478e1e0c7153f4b10a6
PhamTrung99 <35862709+PhamTrung99@users.noreply.github.com>
2020-05-28 10:18:24 +0700

    Lam Trang Danh Muc Khoa hoc

--- Projects/Elearning/Models/Models.js
+++ Projects/Elearning/Models/Models.js
@@ -4,5 +4,10 @@ module.exports = {
         const sql = `select * from elearning.authors` ;
         const rows = db.load(sql);
         return rows;
+    },
+    GetCourseListbyIDSubject: async (IDSubject) => {
+        const sql = `select * from subject_course, courses, subjects,author_course,authors where subject_course.course_id = courses.course_id and subject_course.subject_id = '${IDSubject}' and subjects.subject_id = '${IDSubject}'  and courses.course_id = author_course.course_id and authors.author_id = author_course.author_id`;
+        const rows = db.load(sql);
+        return rows;
     }
 }
